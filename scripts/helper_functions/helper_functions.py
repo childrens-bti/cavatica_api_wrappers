02fedc9abd6a5d6927f57b06e40f4d716b294c88
Alex Sickler <sickler.alex@gmail.com>
2026-07-28 10:22:54 -0400

    :fire: remove unneeed print statements from helper_functions

--- scripts/helper_functions/helper_functions.py
+++ scripts/helper_functions/helper_functions.py
@@ -133,16 +133,13 @@ def get_file_obj(api, project, file_name) -> str:
     Returns:
     - file_obj: api file object
     """
-    print(f"Searching {project} for {file_name}")
     file_obj = None
 
     # first search for the file directly
     files = api.files.query(project=project, names=[file_name])
     if len(files) == 0:
         found_files = []
-        print(
-            f"File {file_name} not found in root dir of {project}, searching within folders"
-        )
+
         # search for the file in the project
 
         # get all files in the project
@@ -255,13 +252,11 @@ def get_all_billing(api):
     """
     Get all billing groups the user has access to.
     """
-    print("Finding billing groups")
     billings = []
     received = LIMIT
     billing_page = api.billing_groups.query(limit=LIMIT)
     billings.extend(billing_page)
     while received < billing_page.total:
-        print(f"Looking for more projects, found {received}")
         billing_page = api.billing_groups.query(limit=LIMIT, offset=received)
         billings.extend(billing_page)
         received += LIMIT
